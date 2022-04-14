816d76eb57a80723b1f91451c8e75f1041cf30b2
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-04-14 20:07:21 -0300

    [UPDATE] Adionando classe PersonagemHabilidades de novo

--- Models/Personagem.cs
+++ Models/Personagem.cs
@@ -1,4 +1,6 @@
 using RpgApi.Models.Enuns;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
 
 namespace RpgApi.Models
 {
@@ -12,6 +14,10 @@ namespace RpgApi.Models
         public int Inteligencia { get; set; }
         public ClasseEnum Classe { get; set; }
         public byte[] FotoPersonagem { get; set; }
+        [JsonIgnore]
         public Usuario Usuario { get; set; }
+        [JsonIgnore]
+        public Arma Arma { get; set; }
+        public List<PersonagemHabilidade> PersonagemHabilidades { get; set; }
     }
 }
