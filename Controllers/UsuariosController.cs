b178e24a6a614551fc11d92784a0c3cb93f83f56
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-04-14 22:11:59 -0300

    [FEAT] Adicionando método para registrar um usuário

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using RpgApi.Data;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using RpgApi.Models;
+using RpgApi.Utils;
 
 namespace RpgApi.Controllers
 {
@@ -28,6 +30,30 @@ namespace RpgApi.Controllers
             }    
         return false;
             
+        }
+
+        // Método para registrar um usuário, caso o username não exista no banco de dados
+        [HttpPost("Registrar")]
+        public async Task<IActionResult> RegistrarUsuario(Usuario user)
+        {
+            try
+            {
+                if(await UsuarioExistente(user.Username))
+                    throw new System.Exception("Nome de usuário já existe");
+
+                    Criptografia.CriarPasswordHash(user.PasswordString, out byte[] hash, out byte[] salt);
+                    user.PasswordString = string.Empty;
+                    user.PasswordHash = hash;
+                    user.PasswordSalt = salt;
+                    await _context.Usuarios.AddAsync(user);
+                    await _context.SaveChangesAsync();
+
+                    return Ok(user.Id);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
 
