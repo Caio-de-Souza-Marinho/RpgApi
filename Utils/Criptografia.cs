4b2194fb01b3e7e1a3c3e71545bcbc6c18dfea60
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-04-14 22:20:12 -0300

    [FEAT] Adicionando método para comparar senha com senha do banco de dados

--- Utils/Criptografia.cs
+++ Utils/Criptografia.cs
@@ -10,5 +10,23 @@ namespace RpgApi.Utils
                 hash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
             }
         }
+
+        //Método que recebe a senha como uma string simples e compara com o que está na base de dados.
+        public static bool VerificarPasswordHash(string password, byte[] hash, byte[] salt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(salt))
+            {
+                var computeHash = 
+        hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i=0; i < computeHash.Length; i++)
+                {
+                    if (computeHash[i] != hash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
     }
 }
