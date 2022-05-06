a4fb02f261674f4d1ef6937d47d11d5873dbd0ca
Caio de Souza Marinho <caiosouzamarinho@gmail.com>
2022-05-06 17:23:52 -0300

    [FEAT] Adicionando método que busque a Id do usuário coletando os dados das claims

--- Controllers/ArmasController.cs
+++ Controllers/ArmasController.cs
@@ -4,18 +4,29 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RpgApi.Data;
 using RpgApi.Models;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace RpgApi.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class ArmasController : ControllerBase
     {
         private readonly DataContext _context;//Declaração contexto do Banco
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public ArmasController(DataContext context)
+        public ArmasController(DataContext context, IHttpContextAccessor httpContextAccessor)
         {
             _context = context; //inicialização do contexto do banco
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        private int ObterUsuarioId()
+        {
+            return int.Parse(_httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier));
         }
 
         [HttpGet("{id}")] //Buscar pelo id
