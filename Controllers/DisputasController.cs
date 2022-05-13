35304180bcbe97c54d10c8f3093da5a8b86b470a
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-05-12 21:47:27 -0300

    [FEAT] Adicionando nova controller Disputas

---
+++ Controllers/DisputasController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using RpgApi.Data;
+
+namespace RpgApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DisputasController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public DisputasController(DataContext context)
+        {
+            _context = context;
+        }
+    }
+}
