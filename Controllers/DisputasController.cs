28be28994fce45334a90fce4677b0eac88962859
Caio de Souza Marinho <caiosouzamarinho@gmail.com>
2022-05-15 22:33:09 -0300

    [FEAT] Adicionando método que faz uma disputa entre dois personagens com o atacante utilizando uma habilidade

--- Controllers/DisputasController.cs
+++ Controllers/DisputasController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Text;
+using System.Collections.Generic;
 
 namespace RpgApi.Controllers
 {
@@ -65,7 +66,62 @@ namespace RpgApi.Controllers
             }
         }
     
-    
+    //Método que fará um ataque a um outro personagem usando uma Habilidade
+    [HttpPost("Habilidade")]
+        public async Task<IActionResult> AtaqueComHabilidadeAsync(Disputa d)
+        {
+            try
+            {
+                Personagem atacante = await _context.Personagens
+                    .Include(p => p.PersonagemHabilidades).ThenInclude(ph => ph.Habilidade)
+                    .FirstOrDefaultAsync(p => p.Id == d.AtacanteId);
+
+                Personagem oponente = await _context.Personagens
+                    .FirstOrDefaultAsync(p => p.Id == d.OponenteId);
+
+                //Busca da habilidade através do ID da habilidade contida no objeto recebido 
+                //por parâmetro na requisição
+                PersonagemHabilidade ph = await _context.PersonagemHabilidades
+                    .Include(p => p.Habilidade)
+                    .FirstOrDefaultAsync(phBusca => phBusca.HabilidadeId == d.HabilidadeId);
+
+                if (ph == null)
+                    d.Narracao = $"{atacante.Nome} não possui esta habilidade";
+                else
+                {
+                    int dano = ph.Habilidade.Dano + (new Random().Next(atacante.Inteligencia));
+                    dano = dano - new Random().Next(oponente.Defesa);
+
+                    if (dano > 0)
+                        oponente.PontosVida = oponente.PontosVida - dano;
+                    if (oponente.PontosVida <= 0)
+                        d.Narracao += $"{oponente.Nome} foi derrotado!";
+
+                    _context.Personagens.Update(oponente);
+                    await _context.SaveChangesAsync();
+
+                    StringBuilder dados = new StringBuilder();
+                    dados.AppendFormat(" Atacante: {0}. ", atacante.Nome);
+                    dados.AppendFormat(" Oponente: {0}. ", oponente.Nome);
+                    dados.AppendFormat(" Pontos de vida do atacante: {0}. ", atacante.PontosVida);
+                    dados.AppendFormat(" Pontos de vida do oponente: {0}. ", oponente.PontosVida);
+                    dados.AppendFormat(" Habilidade utilizada: {0}. ", ph.Habilidade.Nome);
+                    dados.AppendFormat(" Dano: {0}. ", dano);
+
+                    d.Narracao += dados.ToString();
+                    d.DataDisputa = DateTime.Now;
+                    _context.Disputas.Add(d);
+                    _context.SaveChangesAsync();
+                }
+
+                return Ok (d);
+
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+    }
     
     
     
